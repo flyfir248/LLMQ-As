cec4263847325a1537ac69f8112fd927b32f0e1f
@johnya0 <anoopjohny2000@gmail.com>
2023-05-05 14:08:45 +0200

    commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, render_template
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering
+
+app = Flask(__name__)
+
+# Step 1: Choose a pre-trained model architecture
+model_name = "bert-large-uncased-whole-word-masking-finetuned-squad"
+
+# Step 3: Load the pre-trained model
+try:
+    model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+except OSError:
+    print(f"Model not found locally. Downloading {model_name}...")
+    model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+
+# Step 4: Define a tokenizer
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+def clean_text(text):
+    """
+    Removes special characters from the given text
+    """
+    cleaned_text = ""
+    for char in text:
+        if char.isalnum() or char.isspace():
+            cleaned_text += char
+    return cleaned_text
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        file = request.files["file"]
+        text = file.read().decode("utf-8")
+        cleaned_text = clean_text(text)
+
+        question = request.form["question"]
+
+        inputs = tokenizer.encode_plus(question, cleaned_text, return_tensors="pt", max_length=512, truncation=True)
+
+        outputs = model(**inputs)
+
+        answer_start = outputs.start_logits.argmax(dim=-1).item()
+        answer_end = outputs.end_logits.argmax(dim=-1).item()
+        answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(inputs["input_ids"][0][answer_start:answer_end+1]))
+
+        return render_template("result.html", question=question, answer=answer)
+    else:
+        return render_template("question.html")
+
+if __name__ == "__main__":
+    app.run()
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Question Answering Web App
+This is a Flask-based web app that uses the Hugging Face Transformers library to answer questions based on a given text document.
+
+# Requirements
+To run this web app, you will need to have Python 3.6 or higher installed on your system, along with the Flask and transformers libraries. You can install these libraries using pip:
+
+``
+pip install flask transformers
+``
+
+# Usage
+To start the web app, run the following command in your terminal:
+
+``
+python main.py
+``
+
+This will start a Flask server that you can access at http://localhost:5000 in your web browser.
+
+To use the web app, upload a text file using the form on the home page, then type in a question you'd like answered based on the text in the file. Click the "Submit" button to see the answer.
+
+# Credits
+This web app was created using the Hugging Face Transformers library and the Flask web framework.
+
+# License
+This project is licensed under the MIT License - see the LICENSE file for details.
